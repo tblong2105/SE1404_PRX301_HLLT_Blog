dfe7f9a656b1fb0ebad73235945fab5cccb05eb8
Trinh Tran Cong <trinh.trancong@petronas.com>
2022-03-21 23:07:07 +0700

    create repository

---
+++ SE1404_PRX301_HLLT_Project/src/main/java/com/se1404_prx301_hllt_project/SE1404PRX301HLLTProjectApplication.java
@@ -0,0 +1,13 @@
+package com.se1404_prx301_hllt_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SE1404PRX301HLLTProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SE1404PRX301HLLTProjectApplication.class, args);
+	}
+
+}
---
+++ SE1404_PRX301_HLLT_Project/src/test/java/com/assigment/de140022/De140022PracticalexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.de140022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class De140022PracticalexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
